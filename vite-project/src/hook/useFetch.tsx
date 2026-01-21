4d2c49913dfeeb159fc9fb9adfa0cbfa92c6ac9d
Kishan Dervaliya <kishan.dervaliya@roimaint.com>
2026-01-21 18:27:40 +0530

    Modified fetch hook

--- vite-project/src/hook/useFetch.tsx
+++ vite-project/src/hook/useFetch.tsx
@@ -1,32 +1,32 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 
-function useFetch(url:string) {
-    const [data, setData] = useState();
+function useFetch() {
+    // const [data, setData] = useState();
     const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<string>('');
 
-    useEffect(()=>{
-        const fetchData = async ()=>{
-            setLoading(true);
-            try{
-                const ret = await fetch(url);
-                console.log(url)
-                const res = await ret.json();
-                console.log(res)
-                if(!ret.ok)
-                    throw new Error('Return Bad');
-                setData(res);
-            }catch(e){
-                if(e instanceof Error)
-                    setError(e.message);
-                else setError('this is default error');
-            }finally{
-               setLoading(false); 
-            }
+    const fetchData = useCallback(async (url: string) => {
+        setLoading(true);
+        try {
+            const ret = await fetch(url);
+            console.log(url)
+            const res = await ret.json();
+            console.log(res)
+            if (!ret.ok)
+                throw new Error('Return Bad');
+            return res;
+        } catch (e) {
+            if (e instanceof Error)
+                setError(e.message);
+            else setError('this is default error');
+            throw error;
+        } finally {
+            setLoading(false);
         }
-        fetchData();
-    },[url])
-  return {data, loading, error};
+    }, [])
+    // fetchData();
+    // },[url])
+    return { fetchData, loading, error };
 }
 
 export default useFetch
